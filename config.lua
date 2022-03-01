08328dfef001366ba70d27ee0ab56d2e12e010e2
Adam <87525637+sadam376@users.noreply.github.com>
2022-03-01 22:03:59 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,32 @@
+AAP = {}
+
+AAP.AdminGroups = {
+    ['admin'] = true,
+    ['superadmin'] = true
+}
+
+AAP.AdminCommands = {
+    {
+        name = "goto",
+        description = "Teleport to player"
+    },
+    {
+        name = "revive",
+        description = "Player revive"
+    },
+    {
+        name = "kill",
+        description = "Kill the player"
+    },
+    {
+        name = "bring",
+        description = "Teleport player to me"
+    },
+    {
+        name = "noclip",
+        description = "Enable/disable Admin fly"
+    },
+
+}
+
+AAP.DiscordWebhook = "https://discord.com/api/webhooks/948303336560529459/kt1T3h_JpbY-pz4HvMS9RUqTvZUStqHVS8XQCXZfmIDeUN97zmG6RLigSzzAHWB4JvAy"
